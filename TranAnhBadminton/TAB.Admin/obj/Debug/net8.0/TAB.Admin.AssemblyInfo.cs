76c6591d4dd6e9da71e86ddf547acf5207bc55b4
Kudo69 <kudoshinichi2804@gmail.com>
2024-04-04 18:34:32 +0700

    Fix Upload Product Img in view

--- TranAnhBadminton/TAB.Admin/obj/Debug/net8.0/TAB.Admin.AssemblyInfo.cs
+++ TranAnhBadminton/TAB.Admin/obj/Debug/net8.0/TAB.Admin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TAB.Admin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ced345e93c0ee5e8946caf7a6857ff3a326c0f65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8139d97fef10afa63b34777073469822aa09eb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TAB.Admin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TAB.Admin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
